c0c598fc6fda86b1500a36fabe9698ca9dbb7309
Prerit007 <75621370+Prerit007@users.noreply.github.com>
2022-07-04 02:44:58 +0530

    commit

---
+++ components/index.js
@@ -0,0 +1,10 @@
+import Main from "./Main";
+import Card from "./Card";
+import Cards from "./Cards";
+import Transaction from "./Transaction";
+import BuyModal from "./BuyModal";
+import Featured from "./Featured";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+export { Main, Header, Card, Cards, Transaction, BuyModal, Featured, Sidebar };
